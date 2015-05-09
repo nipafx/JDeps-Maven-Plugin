d3f6fde9fc207ccab90f7c07ece00d29313ef551
nicolaiparlog <nipa@codefx.org>
2015-05-09 08:52:11 +0200

    Create a search for JDeps which is composed of the other search strategies

---
+++ src/main/java/org/codefx/maven/plugin/jdeps/find/ComposedJDepsSearch.java
@@ -0,0 +1,36 @@
+package org.codefx.maven.plugin.jdeps.find;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+/**
+ * Tries all available searches to locate JDeps.
+ */
+public class ComposedJDepsSearch implements JDepsSearch {
+
+	private final JavaHomeSystemPropertyJDepsSearch systemPropertySearch;
+
+	private final JavaHomeEnvironmentVariableJDepsSearch environmentVariableSearch;
+
+	/**
+	 * Creates a new search.
+	 */
+	public ComposedJDepsSearch() {
+		systemPropertySearch = new JavaHomeSystemPropertyJDepsSearch();
+		environmentVariableSearch = new JavaHomeEnvironmentVariableJDepsSearch();
+	}
+
+	@Override
+	public Optional<Path> search() {
+		Optional<Path> viaSystemProperty = systemPropertySearch.search();
+		if (viaSystemProperty.isPresent())
+			return viaSystemProperty;
+
+		Optional<Path> viaEnvironmentVariable = environmentVariableSearch.search();
+		if (viaEnvironmentVariable.isPresent())
+			return viaEnvironmentVariable;
+
+		return Optional.empty();
+	}
+
+}
