2f8c275908457b40bb1c15b479db6f6d9a9d859a
nicolaiparlog <nipa@codefx.org>
2015-11-09 17:41:19 +0100

    Create a 'DependencyJudgeBuilder' to allow decoupling from choosing the judge and creating it

---
+++ src/main/java/org/codefx/maven/plugin/jdeps/rules/DependencyJudgeBuilder.java
@@ -0,0 +1,40 @@
+package org.codefx.maven.plugin.jdeps.rules;
+
+/**
+ * Builds a {@link DependencyJudge}.
+ */
+public interface DependencyJudgeBuilder {
+
+	/**
+	 * Sets the specified severity as the default severity used by the created judge.
+	 *
+	 * @param defaultSeverity
+	 * 		the default severity to set
+	 *
+	 * @return this builder
+	 */
+	DependencyJudgeBuilder withDefaultSeverity(Severity defaultSeverity);
+
+	/**
+	 * Adds the specified dependency to the created judge.
+	 *
+	 * @param dependentName
+	 * 		fully qualified name of the type or package which depends on the the other
+	 * @param dependencyName
+	 * 		fully qualified name of the type or package upon which the {@code dependent} depends
+	 * @param severity
+	 * 		the severity of the dependency {@code dependent -> dependency}
+	 *
+	 * @return this builder
+	 */
+	DependencyJudgeBuilder addDependency(String dependentName, String dependencyName, Severity severity);
+
+	/**
+	 * @return a new dependency judge
+	 *
+	 * @throws IllegalStateException
+	 * 		if called more than once
+	 */
+	DependencyJudge build();
+
+}
