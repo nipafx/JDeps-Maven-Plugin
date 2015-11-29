15191d8f696b586f6f14458a0bc7a7b1e7c3dcf1
nicolaiparlog <nipa@codefx.org>
2015-11-29 13:37:27 +0100

    Add stand-alone test project for integration tests

---
+++ src/test/resources/test-project/src/main/java/org/codefx/maven/plugin/testproject/OnBASE64.java
@@ -0,0 +1,18 @@
+package org.codefx.maven.plugin.testproject;
+
+import java.io.IOException;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class OnBASE64 {
+
+	public static void main(String[] args) throws IOException {
+		BASE64Decoder decoder = null;
+		decoder.decodeBuffer((String) null);
+
+		BASE64Encoder encoder = null;
+		encoder.encode((byte[]) null);
+	}
+
+}
